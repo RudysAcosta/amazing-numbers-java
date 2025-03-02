e07b9eb2021b1d803771edb6f88822550fb72de1
Rudys Acosta <ncrousset926@gmail.com>
2025-03-01 21:49:20 -0500

    Refactory test with parameterzedTest

--- src/test/java/dev/ncrousset/model/NumberPropertiesTest.java
+++ src/test/java/dev/ncrousset/model/NumberPropertiesTest.java
@@ -1,22 +1,20 @@
 package dev.ncrousset.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberPropertiesTest {
 
-    @Test
-    void testIsEven() {
-        int[] numbers = {2,4,6,8,20, 100, 36, 78, 92, 76, };
+    @ParameterizedTest
+    @ValueSource(ints = {2,4,6,8,20, 100, 36, 78, 92, 76,})
+    void testIsEven(int num) {
+        Number number = new Number(num);
+        NumberProperties numberProperties = new NumberProperties(number);
 
-        for (int num: numbers) {
-            Number number = new Number(num);
-            NumberProperties numberProperties = new NumberProperties(number);
-
-            assertTrue(numberProperties.isEven());
-        }
+        assertTrue(numberProperties.isEven());
     }
-
-
 
 }
