0422238048a140429b31fb13fb8f3b47c4072990
Rudys Acosta <ncrousset926@gmail.com>
2025-03-02 12:58:02 -0500

    Remove \n of Welcome mesage

--- src/main/java/dev/ncrousset/Main.java
+++ src/main/java/dev/ncrousset/Main.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Amazing Numbers!\n");
+        System.out.println("Welcome to Amazing Numbers!");
         printSupportedRequests();
 
 
